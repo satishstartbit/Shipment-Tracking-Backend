72cd296d79408ae68ee47adbe63342fe0eff84b0
jai <jai@ouverture.ai>
2025-04-02 17:22:46 +0530

    secured api

--- routes/AdminuserRoutes.js
+++ routes/AdminuserRoutes.js
@@ -1,29 +1,30 @@
 const express = require('express');
-const { registerUser, getUserById, getAllUsers, deleteUserById , editUserDetails} = require('../controllers/AdminCreateUsers');
+const { registerUser, getUserById, getAllUsers, deleteUserById, editUserDetails } = require('../controllers/AdminCreateUsers');
 const { loginUser, refreshAccessToken } = require('../controllers/AdminAuthController');
+const verifyToken = require('../utils/VerifyToken'); // Import the verifyToken middleware
 
 const router = express.Router();
 
 // POST route to register a new user
-router.post('/registeruser', registerUser);
-router.post('/updateuser', editUserDetails);
+router.post('/registeruser', verifyToken, registerUser);
+router.post('/updateuser', verifyToken, editUserDetails);
 
 
 // GET route to get user details by ID
-router.get('/:id', getUserById);
+router.get('/:id', verifyToken, getUserById);
 
 
 
 // Delete route to get user details by ID
-router.get('/delete/:id', deleteUserById);
+router.get('/delete/:id', verifyToken, deleteUserById);
 
 // POST route to login a user and get access & refresh tokens
 router.post('/login', loginUser);
 
 // POST route to refresh the access token using the refresh token
-router.post('/refresh', refreshAccessToken);
+router.post('/refresh', verifyToken, refreshAccessToken);
 
 // GET route to fetch all users
-router.get('/', getAllUsers);
+router.get('/', verifyToken, getAllUsers);
 
 module.exports = router;
