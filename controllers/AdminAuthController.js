c09161097d3bb54083ffc56f31fde16ad288f06a
jai <jai@ouverture.ai>
2025-04-07 14:44:18 +0530

    Update AdminAuthController.js

--- controllers/AdminAuthController.js
+++ controllers/AdminAuthController.js
@@ -6,10 +6,9 @@ const { generateRefreshToken } = require('../utils/generateRefreshToken');
 // Controller to login the user and return both access and refresh tokens
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
-  let isemail = email?.toLowerCase()
   try {
     // Check if the user exists
-    const user = await Users.findOne({ isemail });
+    const user = await Users.findOne({ email });
 
     if (!user) {
       return res.status(400).json({ message: 'Invalid email or password' });
